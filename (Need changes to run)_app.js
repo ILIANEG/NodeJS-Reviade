d5a84884009bf232420711cc63d0d0f07d10f537
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2021-01-14 11:27:53 -0500

    Add files via upload

---
+++ (Need changes to run)_app.js
@@ -0,0 +1,33 @@
+/* This file puts all elements of application together */
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const ownerRoutes = require('./routes/ownerRoutes');
+const publicRouter = require('./routes/publicRoutes');
+const cookieParser = require('cookie-parser');
+
+
+mongoose.set('useFindAndModify', false);
+
+//initialize express app
+const app = express();
+
+//app.use(express.urlencoded());
+app.use(express.json());
+app.use(cookieParser());
+
+//specify static content
+app.use(express.static(__dirname + '/public'));
+
+//specify view engine
+app.set('view engine', 'ejs');
+
+//Connect to the server
+const dbURL = 'mongodb+srv://illia:3244Ilija89@cluster0.yl2os.mongodb.net/reviade?retryWrites=true&w=majority';
+mongoose.connect(dbURL).then(app.listen(3000)).catch((error) => {
+    console.log(error);
+});
+
+app.use(authRoutes);
+app.use(ownerRoutes);
+app.use(publicRouter);
