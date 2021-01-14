ad7f784235a03fb43594ef95a7950bb435cb4545
Illia Negovora <illian@illiaLaptop>
2021-01-14 18:52:45 -0500

    add review functionality is implemented

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,24 +1,32 @@
 const { Product, Comment, Feedback } = require('../models/Product');
+const { findByIdAndUpdate } = require('../models/User');
 
 module.exports.menu_get = async (req, res) => {
     const id = req.params.id;
     const product = await Product.findById(id);
-    res.status(201).render('publicMenu', {title: 'Menu', productName: product.productName, id: id});
+    res.status(200).render('publicMenu', {title: 'Menu', productName: product.productName, id: id});
 }
 
 module.exports.reviews_get = async (req, res) => {
     const id = req.params.id;
-    res.send('get comments');
+    const product = await Product.findById(id).populate('comments');
+    res.status(200).render('listReviews', {title:'Reviews', id: id, reviews: product.comments});
 }
 
 module.exports.new_review_get = async (req, res) => {
     const id = req.params.id;
-    res.send('comment get form');
+    res.status(200).render('addReview', {title:'Add Review', id:id});
 }
 
 module.exports.new_review_post = async (req, res) => {
     const id = req.params.id;
-    res.send('comment post form');
+    console.log(req.body);
+    const review = req.body;
+    const newComment = await Comment.create(review);
+    const product = await Product.findById(id);
+    product.comments.push(newComment.id);
+    await product.save();
+    res.status(201).json({message:'success'});
 }
 
 module.exports.feedback_get = async (req, res) => {
